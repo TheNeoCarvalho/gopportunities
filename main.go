279bbe8efd506cf137c13829ec3ead94396e061e
Manoel Carvalho <manoelmecat@gmail.com>
2023-08-11 17:02:31 -0300

    feat: add swagger configuration

--- main.go
+++ main.go
@@ -5,17 +5,20 @@ import (
 	"github.com/TheNeoCarvalho/gopportunities/router"
 )
 
+
 var (
 	logger *config.Logger
 )
+
 func main() {
 	logger = config.GetLogger("main")
+	// Initialize Configs
 	err := config.Init()
-
 	if err != nil {
-		logger.Errorf("Config initialization error: %v", err)
+		logger.Errorf("config initialization error: %v", err)
 		return
 	}
 
+	// Initialize Router
 	router.Init()
 }
--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,19 @@
 package router
 
 import (
+	docs "github.com/TheNeoCarvalho/gopportunities/docs"
 	"github.com/TheNeoCarvalho/gopportunities/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initRoutes(router *gin.Engine){
 	handler.InitHandler()
-		v1:= router.Group("/api/v1")
+	basePath := "/api/v1"
+
+	docs.SwaggerInfo.BasePath = basePath
+	v1:= router.Group("/api/v1")
 		{
 			v1.GET("/opening/:id", handler.ShowOpeningHandler)
 			v1.POST("/opening", handler.CreateOpeningHandler)
@@ -15,4 +21,5 @@ func initRoutes(router *gin.Engine){
 			v1.PUT("/opening/:id", handler.UpdateOpeningHandler)
 			v1.GET("/openings", handler.ListOpeningsHandler)
 		}
+		router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
